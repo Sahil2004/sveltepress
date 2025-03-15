c14241cf5a1f8d594c8526239185bcc156c13155
Sahil Garg <me.sahil.gg@gmail.com>
2025-03-16 00:45:02 +0530

    feat: added rest api endpoints metrics

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,16 @@
 import express from "express";
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { PORT, DATABASE_URL } from "@/config";
-
+import { initRestMetrics } from "@/utils";
 
 const app = express();
 const db = drizzle(DATABASE_URL);
+
+app.use(initRestMetrics)
+
+app.get("/", (req, res) => {
+	res.send("Hello World");
+})
 
 app.listen(PORT, () => {
 	console.log(`Server is running on http://localhost:${PORT}`);
